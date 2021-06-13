3a1939d9d68e5bbd67cef3758e3ac9f46bfc5491
gokul <gokul.0369@gmail.com>
2021-06-13 17:40:32 +0530

    Update run_demo.py

--- scripts/run_demo.py
+++ scripts/run_demo.py
@@ -3,6 +3,13 @@ import cv2
 import argparse
 import numpy as np
 import pyfakewebcam
+import multiprocessing as mp
+import zmq
+import random
+import sys
+import cv2
+from simplejpeg import decode_jpeg, encode_jpeg
+import msgpack
 
 from configs.color_config import ColorConfig
 from configs.model_config import detector, landmark_predictor
@@ -17,38 +24,84 @@ def shape_to_np(shape, dtype="int"):
         coords[i] = (shape.part(i).x, shape.part(i).y)
     return coords
 
+def pack_message(msg):
+    return msgpack.packb(msg)
+
+def unpack_message(msg):
+    return msgpack.unpackb(msg)
+
+def predict(frame, socket, cv2):
+    frame = cv2.resize(frame, (640, 480))  # resize the frame
+    socket.send(pack_message(encode_jpeg(frame, quality = 100, colorspace = "RGB", fastdct=True)))
+
+    msg = socket.recv()
+
+    source = decode_jpeg(unpack_message(msg), colorspace = "RGB", fastdct=True, fastupsample = True)
+    # source = cv2.resize(source, (480, 360))  # resize the frame
+    cv2.imshow('image',source)
+    cv2.waitKey(1)
+    #time.sleep(1)
+    camera.schedule_frame(source)
+
 
 def run_webcam(args):
-    camera = pyfakewebcam.FakeWebcam('/dev/video7', 640, 480) 
-    vis_type = args.get('vis_type', None)
-    out_type = args.get('out_type', None)
-    background_img = cv2.imread(f"backgrounds/bg{args['background_image']}.jpg")
+    if (args.get('remote_server', 0) == 0):
+        camera = pyfakewebcam.FakeWebcam('/dev/video9', 640, 480) 
+        vis_type = args.get('vis_type', None)
+        out_type = args.get('out_type', None)
+        background_img = cv2.imread(f"backgrounds/bg{args['background_image']}.jpg")
+        background_img = cv2.cvtColor(background_img, cv2.COLOR_BGR2RGB)
+        dp_predictor = Predictor(visualizer_type=vis_type, output_type=out_type)
+        stream = WebcamVideoStream(src=0).start()
+        time.sleep(1.0)
+        camera_started_flag = True
+        try:
+            while True:
+                frame = stream.read()
+                background_img = cv2.resize(background_img, (frame.shape[1], frame.shape[0]), interpolation = cv2.INTER_AREA)
 
-    background_img = cv2.cvtColor(background_img, cv2.COLOR_BGR2RGB)
-    dp_predictor = Predictor(visualizer_type=vis_type, output_type=out_type)
-    stream = WebcamVideoStream(src=0).start()
-    time.sleep(1.0)
-    cnt = 0
-    camera_started_flag = True
-    while True:
-        frame = stream.read()
-        background_img = cv2.resize(background_img, (frame.shape[1], frame.shape[0]), interpolation = cv2.INTER_AREA)
-        out = dp_predictor.dp_predict(frame, background_img)
-        # combined_image = np.concatenate((frame, out), axis=1)
-        # cv2.imshow('frame', combined_image)
+                out = dp_predictor.dp_predict(frame, background_img)
 
-        # 2nd Camera ouput
-        out=cv2.resize(out, (640, 480), interpolation=cv2.INTER_AREA)
-        out = cv2.cvtColor(out, cv2.COLOR_BGR2RGB)
-        camera.schedule_frame(out)
-        if camera_started_flag:
-            print("Camera started ...")
-            camera_started_flag = False
+                combined_image = np.concatenate((frame, out), axis=1)
+                out = cv2.cvtColor(out, cv2.COLOR_BGR2RGB)
 
-        # fps.update()
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+                cv2.imshow('image',out)
+                cv2.waitKey(1)
+                camera.schedule_frame(out)
 
+                if camera_started_flag:
+                    print("Camera started ...")
+                   camera_started_flag = False
+        except KeyboardInterrupt:
+            context.destroy()
+                
+    else:
+        context = zmq.Context()
+        socket = context.socket(zmq.PAIR)
+        socket.connect(args.get('remote_server_ip'))
+        stream = WebcamVideoStream(src=0).start()
+        camera = pyfakewebcam.FakeWebcam('/dev/video9', 640, 480)
+        camera_started_flag = True 
+        try:
+            while True:
+                    frame = stream.read()
+                    frame = cv2.resize(frame, (640, 480))  # resize the frame
+                    socket.send(pack_message(encode_jpeg(frame, quality = 100, colorspace = "RGB", fastdct=True)))
+
+                    msg = socket.recv()
+
+                    source = decode_jpeg(unpack_message(msg), colorspace = "RGB", fastdct=True, fastupsample = True)
+
+                    cv2.imshow('image',source)
+                    cv2.waitKey(1)
+                    camera.schedule_frame(source)
+
+                    if camera_started_flag:
+                        print("Camera started ...")
+                        camera_started_flag = False
+
+        except KeyboardInterrupt:
+            context.destroy()
 
 
 if __name__ == '__main__':
@@ -63,5 +116,11 @@ if __name__ == '__main__':
     # follow naming convention bg{img_no}.jpg, eg: bg1.jpg
     ap.add_argument("-bg", "--background-image", type=int, default=1,
                     help="Background image no.")
+
+    ap.add_argument("-rs","--remote_server", type=int, default=0,
+                    help="remote type (0, 1")
+
+    ap.add_argument("-rsip","--remote_server_ip", help="tcp ip address port, ex: tcp://0.tcp.ngrok.io:13261")
+
     args = vars(ap.parse_args())
     run_webcam(args)
